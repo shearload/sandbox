fa89a2d84cf0229f94d77675f8e32ec592194289
shearload <wyethbinder@gmail.com>
2020-10-20 13:19:40 -0700

    Update hello_world.py
    updates to hello world

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,14 @@
 #It takes time to get started, but we're rollin now
 new_msg = 'Hello World!'
 newer_msg = 'Can you hear me Major Tom?'
+newest_msg = 'Prepare for liftoff'
+
+
 print(new_msg)
 print(newer_msg)
+
+for i in range(11):
+	print("Countdown: ", i)
+	i = i + 1
+
+print(newest_msg)
